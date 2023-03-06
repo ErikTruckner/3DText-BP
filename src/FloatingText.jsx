a9887f14a222d987ba4c19ea05baed9527f98a08
Erik Truckner <eriktruckner@comcast.net>
2023-03-05 20:18:05 -0500

    click text to change it

--- src/FloatingText.jsx
+++ src/FloatingText.jsx
@@ -1,9 +1,10 @@
-import React, { useRef } from 'react'
+import React, { useState, useRef } from 'react'
 import { useFrame } from '@react-three/fiber'
 import { Center, Text3D } from '@react-three/drei'
 
 const FloatingText = () => {
   const meshRef = useRef()
+  const [text, setText] = useState('HELLO R3F')
 
   useFrame(({ clock }) => {
     const t = clock.getElapsedTime()
@@ -12,12 +13,21 @@ const FloatingText = () => {
     meshRef.current.position.z = Math.sin(t) * 0.5
   })
 
+  const handleClick = () => {
+    setText((prevText) =>
+      prevText === 'HELLO R3F' ? 'HELLO WORLD' : 'HELLO R3F'
+    )
+  }
+
   return (
     <>
       {/* ... */}
       <Center>
-        <Text3D ref={meshRef} font='\fonts\Merriweather_Regular.json'>
-          HELLO R3F
+        <Text3D
+          ref={meshRef}
+          font='\fonts\Merriweather_Regular.json'
+          onClick={handleClick}>
+          {text}
           <meshNormalMaterial />
         </Text3D>
       </Center>
