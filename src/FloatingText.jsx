ad830cacb40e85596e54d623263c6827842a5a23
Erik Truckner <eriktruckner@comcast.net>
2023-03-05 19:56:27 -0500

    Floating Text

--- src/FloatingText.jsx
+++ src/FloatingText.jsx
@@ -3,11 +3,20 @@ import { useFrame } from '@react-three/fiber'
 import { Center, Text3D } from '@react-three/drei'
 
 const FloatingText = () => {
+  const meshRef = useRef()
+
+  useFrame(({ clock }) => {
+    const t = clock.getElapsedTime()
+    meshRef.current.position.y = Math.sin(t) * 0.2
+    meshRef.current.position.x = Math.sin(t) * 0.5
+    meshRef.current.position.z = Math.sin(t) * 0.5
+  })
+
   return (
     <>
       {/* ... */}
       <Center>
-        <Text3D font='\fonts\Merriweather_Regular.json'>
+        <Text3D ref={meshRef} font='\fonts\Merriweather_Regular.json'>
           HELLO R3F
           <meshNormalMaterial />
         </Text3D>
