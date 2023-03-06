14929d26cafe8d6ec2405f731f48de1b54c94b76
Erik Truckner <eriktruckner@comcast.net>
2023-03-06 05:34:19 -0500

    trying to add controls to cube, not working

--- src/App.jsx
+++ src/App.jsx
@@ -7,14 +7,16 @@ import { OrbitControls } from '@react-three/drei'
 // import ComplexSolarSystem from './ComplexSolarSystem'
 // import { Stars } from '@react-three/drei'
 import FloatingText from './FloatingText'
+import Cube from './Cube'
 
 export default function App() {
   console.log(Canvas)
 
   return (
-    <Canvas>
+    <Canvas onKeyDown={(e) => onKeyDown(e)} onKeyUp={(e) => onKeyUp(e)}>
       <FloatingText />
-      <OrbitControls />
+      <Cube position={[0, 0, 0]} />
+      {/* <OrbitControls /> */}
     </Canvas>
   )
 }
